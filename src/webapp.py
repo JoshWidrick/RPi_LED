50fce251d8dfda04d7049a0fefb51ee2808060f6
Josh Widrick <jjwidric@buffalo.edu>
2020-12-08 11:43:54 -0500

    Further code cleanup

--- src/webapp.py
+++ src/webapp.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, render_template, redirect
 import sys
-from forms import HomeForm
 
 
 sys.stdout.write('*===* STARTING APPLICATION *===* \n')
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'JgMWQ3X2c286Zv2gVqmC6PbPbWAcqZKg'
@@ -20,20 +20,19 @@ def rgb_to_hex(r, g, b):
 def check_status():
     with open("./file/status.txt", "r") as f:
         try:
-            status = f.read().strip().split(',')
-            return status
+            return f.read().strip().split(',')
         except:
             return 'failed'
 
 
 def update_status(mode, r, g, b, brightness, power):
-    try:
-        with open("./file/status.txt", "w") as f:
+    with open("./file/status.txt", "w") as f:
+        try:
             new_status = f'{mode},{r},{g},{b},{brightness},{power}'
             f.write(new_status)
-        return "success"
-    except Exception as e:
-        return "failed"
+            return "success"
+        except Exception as e:
+            return "failed"
 
 
 @app.route('/', methods=('GET', 'POST'))
@@ -45,24 +44,16 @@ def submit():
         new_color_hex = request.form['hexcolor'][1:]
         new_color = hex_to_rgb(new_color_hex)
         new_brightness = request.form['brightness']
-        power = 1
+        default_power = 1
 
         sys.stdout.write('updating status... ')
-        x = update_status(new_effect, new_color[0], new_color[1], new_color[2], new_brightness, power)
+        x = update_status(new_effect, new_color[0], new_color[1], new_color[2], new_brightness, default_power)
         sys.stdout.write(f'{x}. \n')
-
-        print(request.form['effect'])
-        print(request.form['hexcolor'])
 
         return redirect('/')
 
     status = check_status()
     current_color_hex = rgb_to_hex(status[1], status[2], status[3])
-    print(current_color_hex)
-    current_brightness = int(status[4])
-
-    print(status[0])
-
     return render_template('home.html', status=status, current_color_hex=current_color_hex)
 
 

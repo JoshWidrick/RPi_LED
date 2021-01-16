d488f99bd23608eaa957f7b25ad07bcf8be896c9
Joshua Widrick <jjwidric@buffalo.edu>
2021-01-16 13:22:12 -0500

    remade react

--- web/web-react-DEPR/config/webpack.config.js
+++ web/web-react-DEPR/config/webpack.config.js
@@ -561,6 +561,7 @@ module.exports = function (webpackEnv) {
           {
             inject: true,
             template: paths.appHtml,
+            filename: "../../templates/index.html",
           },
           isEnvProduction
             ? {
